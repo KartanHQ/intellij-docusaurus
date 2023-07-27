f2094de9fbde95db14ca393a9a68064f7c0d5c5d
Milad Nekofar <milad@nekofar.com>
2023-07-27 10:45:33 +0330

    refactor: improve project generator to be more concise
    The update in DocusaurusCliProjectGenerator makes the file more concise with code syntax. Unused imports have been removed while others were replaced with their equivalent functions or values. For instance, the project's generator will now generate in a temporary location, changing the generateInTemp() function from 'false' to 'true'. This change will prevent cluttering the project workspace with temporary files. Meanwhile, "DocusaurusBundle.message" replaced "DocusaurusBundle.message()", serving the same purpose but in a more condensed manner.

--- src/main/kotlin/com/nekofar/milad/intellij/docusaurus/cli/DocusaurusCliProjectGenerator.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/docusaurus/cli/DocusaurusCliProjectGenerator.kt
@@ -6,33 +6,30 @@ import com.intellij.lang.javascript.boilerplate.NpxPackageDescriptor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentEntry
 import com.intellij.openapi.vfs.VirtualFile
-import com.nekofar.milad.intellij.docusaurus.DocusaurusBundle
+import com.nekofar.milad.intellij.docusaurus.DocusaurusBundle.message
 import com.nekofar.milad.intellij.docusaurus.DocusaurusIcons
-import javax.swing.Icon
 
 class DocusaurusCliProjectGenerator : NpmPackageProjectGenerator() {
     private val packageName = "create-docusaurus"
     private val npxCommand = "create-docusaurus"
 
-    override fun getName(): String = DocusaurusBundle.message("docusaurus.project.generator.name")
+    override fun getName() = message("docusaurus.project.generator.name")
 
-    override fun getDescription(): String = DocusaurusBundle.message("docusaurus.project.generator.description")
+    override fun getDescription() = message("docusaurus.project.generator.description")
 
-    override fun filters(project: Project, baseDir: VirtualFile): Array<Filter> = emptyArray()
+    override fun filters(project: Project, baseDir: VirtualFile) = emptyArray<Filter>()
 
     override fun customizeModule(virtualFile: VirtualFile, contentEntry: ContentEntry?) {}
 
-    override fun packageName(): String = packageName
+    override fun packageName() = packageName
 
-    override fun presentablePackageName(): String =
-        DocusaurusBundle.message("docusaurus.project.generator.presentable.package.name")
+    override fun presentablePackageName() = message("docusaurus.project.generator.presentable.package.name")
 
     override fun getNpxCommands() = listOf(NpxPackageDescriptor.NpxCommand(packageName, npxCommand))
 
-    override fun generateInTemp(): Boolean = false
+    override fun generateInTemp() = true
 
-    override fun generatorArgs(project: Project?, dir: VirtualFile?, settings: Settings?): Array<String> =
-        arrayOf(".")
+    override fun generatorArgs(project: Project, baseDir: VirtualFile) = arrayOf(project.name)
 
-    override fun getIcon(): Icon = DocusaurusIcons.ProjectGenerator
+    override fun getIcon() = DocusaurusIcons.ProjectGenerator
 }
